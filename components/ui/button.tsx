b286df001ed459f8da277d9dc94b0a770c54a8bd
Jean Carlos Ruiz <66891780+jeancarlosruiz@users.noreply.github.com>
2024-07-21 15:04:13 -0400

    shadcn ui, buttons design

---
+++ components/ui/button.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { cva, type VariantProps } from 'class-variance-authority'
+
+import { cn } from '@/lib/utils'
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center whitespace-nowrap rounded-3xl font-bold ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300',
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-purple-500 text-clr-white hover:bg-purple-200 dark:bg-purple-500 dark:text-clr-white dark:hover:bg-purple-200',
+        destructive:
+          'bg-red-300 text-clr-white hover:bg-red-100 dark:bg-red-300 dark:text-clr-white dark:hover:bg-red-100',
+        outline:
+          'border border-neutral-200 bg-white hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
+        secondary:
+          'bg-white-200 text-btn-s text-purple-500 hover:bg-purple-500/25 dark:bg-white-100 dark:text-purple-500 dark:hover:bg-white-100',
+        ghost:
+          'hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
+        link: 'text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50',
+      },
+      size: {
+        sm: 'h-9 px-3',
+        s: 'h-10 text-sm',
+        l: 'h-12',
+        lg: 'h-11 px-8',
+        icon: 'h-10 w-10',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'l',
+    },
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button'
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = 'Button'
+
+export { Button, buttonVariants }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,45 +6,82 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
+    fontSize: {
+      sm: [
+        '0.8125rem',
+        {
+          lineHeight: '1.4375rem',
+        },
+      ],
+      m: [
+        '0.75rem',
+        {
+          lineHeight: '0.9375rem',
+        },
+      ],
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     screens: {
       sm: '48rem',
     },
-    colors: {
-      purple: {
-        500: 'hsl(242, 48%, 58%)',
-        200: 'hsl(243, 100%, 82%)',
-      },
-      black: {
-        800: 'hsl(237, 100%, 4%)',
-        700: 'hsl(235, 16%, 15%)',
-        600: 'hsl(235, 12%, 19%)',
-      },
-      gray: {
-        500: 'hsl(236, 11%, 27%)',
-        300: 'hsl(216, 15%, 57%)',
-        200: 'hsl(221, 69%, 94%)',
-      },
-      white: {
-        300: 'hsl(220, 69%, 97%)',
-        100: 'hsl(0, 0%, 100%)',
-      },
-      red: {
-        300: 'hsl(0, 78%, 63%)',
-        100: 'hsl(0, 100%, 80%)',
-      },
-      'clr-black': 'hsl(237, 100%, 4%)',
-      'clr-white': 'hsl(0, 0%, 100%)',
-    },
     extend: {
+      colors: {
+        purple: {
+          500: 'hsl(242, 48%, 58%)',
+          200: 'hsl(243, 100%, 82%)',
+        },
+        black: {
+          800: 'hsl(237, 100%, 4%)',
+          700: 'hsl(235, 16%, 15%)',
+          600: 'hsl(235, 12%, 19%)',
+        },
+        gray: {
+          500: 'hsl(236, 11%, 27%)',
+          300: 'hsl(216, 15%, 57%)',
+          200: 'hsl(221, 69%, 94%)',
+        },
+        white: {
+          300: 'hsl(220, 69%, 97%)',
+          200: 'hsl(0, 0, 95)',
+          100: 'hsl(0, 0%, 100%)',
+        },
+        red: {
+          300: 'hsl(0, 78%, 63%)',
+          100: 'hsl(0, 100%, 80%)',
+        },
+        'clr-black': 'hsl(237, 100%, 4%)',
+        'clr-white': 'hsl(0, 0%, 100%)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 }
 export default config
